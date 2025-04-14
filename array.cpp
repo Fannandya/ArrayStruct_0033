67bc9330a1abf8eb6a935a65e5a0c27953d78cf9
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-04-14 11:03:29 +0700

    memberikam nilai pada array

--- array.cpp
+++ array.cpp
@@ -9,3 +9,28 @@ string nama[5]={"ben", "agus", "fara", "ama", "akila"};
 
 // mendeklarasikan array dengan ukuran dan tanpa nilai awal
 float skor[5];
+
+int main (){
+    cout << "nama pada index 4 adalah " << nama[3] << endl;
+    // memberi nilai pada elemen array per index
+    nama[3] = "husna";
+    cout << "nama pada index 4 adalah " << nama[3] << endl;
+
+    cout << "input skor" << endl;
+    cout << endl;
+
+    // memberi nilai awal array dengan looping for
+    for (int i = 0; 1<5; i++){
+        cout << "skor ke- " << i+1 << " =";
+        cin >> skor[i];
+    }
+
+    cout << "menampilkan skor" << endl;
+    cout << endl;
+    
+    // memberi nilai awal array dengan looping for
+    for (int i = 0; 1<5; i++){
+        cout << "skor ke- " << i+1 << " =" << skor[i] << endl;
+    }
+
+}
